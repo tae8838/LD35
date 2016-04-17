e1c54a9968e1a7ca168056ee30928f57c010559b
KristopherPeterson <kristopher.patrick.peterson@gmail.com>
2016-04-17 14:27:48 +0700

    -Add UI prefab and fonts

---
+++ Assets/Code/UI/HUD.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.VersionControl;
+
+public class HUD : MonoBehaviour {
+
+	public Sprite heartSprite;
+	public Sprite heartEmptySprite;
+	public GameObject[] hearts;
+
+	//find out how many hearts are needed from difficulty level, create that many hearts
+	void SetupNumberOfHearts(){
+	}
+
+	//change one of the hearts to a full heart
+	void AddHealth(){
+	}
+
+	//change a heart to an empty heart image
+	void SubtractHealth(){
+
+	}
+
+}

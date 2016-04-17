71f089f6384cba9f8a2296ca82a17d68a83bb07f
KristopherPeterson <kristopher.patrick.peterson@gmail.com>
2016-04-17 15:53:18 +0700

    -Add Gameover UI

---
+++ Assets/Code/UI/Gameover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.VersionControl;
+using UnityEngine.UI;
+
+public class Gameover : MonoBehaviour {
+
+	public Player player;
+	public Text endScoreText;
+
+	void Start(){
+		endScoreText.text = "SCORE: " + player.score;
+	}
+}

ac36bc4caab4ded3432483761d3f4288694fbac6
KristopherPeterson <kristopher.patrick.peterson@gmail.com>
2016-04-19 00:04:47 +0700

    -Work on Tree / Rainbow  -Green spawn

---
+++ Assets/Code/AnimatedUVs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedUVs : MonoBehaviour {
+		
+	float scrollSpeed = 0.2f;
+		
+	void Update()
+	{
+		float offset = Time.time * scrollSpeed;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offset % 1, 0);
+	}
+}
